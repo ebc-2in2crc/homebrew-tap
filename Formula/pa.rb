474ce7d813fa436a834f917465599800e7e336f1
ebc-2in2crc <y015h1n80@gmail.com>
2020-07-11 22:06:47 +0900

    Fix pa SHA256

--- Formula/pa.rb
+++ Formula/pa.rb
@@ -1,8 +1,9 @@
 class Pa < Formula
+  desc "Pixela (https://pixe.la/) CLI tools"
   version '0.9.0'
   homepage 'https://github.com/ebc-2in2crc/pa'
   url "https://github.com/ebc-2in2crc/pa/releases/download/v0.9.0/pa_darwin_amd64.zip"
-  sha256 '5d51444a938ca2cba499289c7b9582264f55e707c8914c226bb7c1e2902b48e9'
+  sha256 '2c1169ee6ba4c38dacf5fbd128d0a9ccfd0c4271ed49d25e8de6e3590801a766'
   head 'https://github.com/ebc-2in2crc/pa.git'
 
   head do
