6cd0bb4ff5e2ee64948d5910c59089edcd91ec50
ebc-2in2crc <y015h1n80@gmail.com>
2021-03-18 02:34:39 +0900

    Update url-anchor v1.2.0

--- Formula/url-anchor.rb
+++ Formula/url-anchor.rb
@@ -1,9 +1,9 @@
 class UrlAnchor < Formula
   desc "Converts URL to HTML `<a>` tag"
-  version '1.1.0'
+  version '1.2.0'
   homepage 'https://github.com/ebc-2in2crc/url-anchor'
-  url "https://github.com/ebc-2in2crc/url-anchor/releases/download/v1.1.0/url-anchor_darwin_amd64.zip"
-  sha256 '98b27c07f528f113f03cfaba266a8557648cbcdd5b34ca8f1a56e9adbee88ab6'
+  url "https://github.com/ebc-2in2crc/url-anchor/releases/download/v1.2.0/url-anchor_darwin_amd64.zip"
+  sha256 'ac73b304df2478e7b8c4f0baa66355416e932d6f4906dff5bf872361fc06ba4a'
   head 'https://github.com/ebc-2in2crc/url-anchor.git'
 
   head do
