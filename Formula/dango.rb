e9c59f3c4c38452990ff1690af1f6588a8d7d4f0
ebc-2in2crc <y015h1n80@gmail.com>
2021-05-05 14:39:36 +0900

    Update dango v1.0.0

--- Formula/dango.rb
+++ Formula/dango.rb
@@ -1,14 +1,14 @@
 class Dango < Formula
   desc "Concatenates and splits standard input"
-  version '0.9.0'
+  version '1.0.0'
   homepage 'https://github.com/ebc-2in2crc/dango'
   if OS.mac?
-    url "https://github.com/ebc-2in2crc/dango/releases/download/v0.9.0/dango_darwin_amd64.zip"
-    sha256 '9072c935d1e17dbff9d16bb406b59c8d7330e0afcbc0c61ec0dd4108566a9652'
+    url "https://github.com/ebc-2in2crc/dango/releases/download/v1.0.0/dango_darwin_amd64.zip"
+    sha256 '660693a01893422f5a1fce243438b0a041abe08e200f884d1bc8d8586cc10324'
   end
   if OS.linux?
-    url "https://github.com/ebc-2in2crc/dango/releases/download/v0.9.0/dango_linux_amd64.zip"
-    sha256 'd2f49ebed9117c751eca1fe58067fa5bb37913687bdf9b6373d123afa15436ef'
+    url "https://github.com/ebc-2in2crc/dango/releases/download/v1.0.0/dango_linux_amd64.zip"
+    sha256 '720f56b0b8802eee35af4b3b2788df16c9b9d949b73105d83a6870ac8f1b30a4'
   end
   head 'https://github.com/ebc-2in2crc/dango.git'
 
