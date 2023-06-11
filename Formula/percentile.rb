8e401be0ec4f2be5c08fc4c36270712e7ac3e5b7
ebc-2in2crc <y015h1n80@gmail.com>
2023-06-11 13:17:33 +0900

    Updater percentile v0.2.1

--- Formula/percentile.rb
+++ Formula/percentile.rb
@@ -1,14 +1,14 @@
 class Percentile < Formula
   desc "CLI tool to calculate percentile from a file"
-  version '0.2.0'
+  version '0.2.1'
   homepage 'https://github.com/ebc-2in2crc/percentile'
   if OS.mac?
-    url "https://github.com/ebc-2in2crc/percentile/releases/download/v0.2.0/percentile_darwin_amd64.zip"
-    sha256 '25751463a0d1622931e38a749d2b5d8b7784406ce56d1c0a1ea1cd0e6c3a4926'
+    url "https://github.com/ebc-2in2crc/percentile/releases/download/v0.2.1/percentile_darwin_amd64.zip"
+    sha256 '5a9b8d8e04fdabbd4eb3dcffe49e5f3ce38bda91177d64c46e3053a5d466b584'
   end
   if OS.linux?
-    url "https://github.com/ebc-2in2crc/percentile/releases/download/v0.2.0/percentile_linux_amd64.zip"
-    sha256 '2df3d1a338b883514daeb32ff3b6aaf9377f38fd3cfd1bc565318f78db2cc475'
+    url "https://github.com/ebc-2in2crc/percentile/releases/download/v0.2.1/percentile_linux_amd64.zip"
+    sha256 'b75e24121aed4feffd8320819e356fc34f96e7188f91bf6828b995624b5d0450'
   end
   head 'https://github.com/ebc-2in2crc/percentile.git'
 
