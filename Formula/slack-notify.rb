b3acb43cd452a694447658861368ef9923b29bc3
ebc-2in2crc <y015h1n80@gmail.com>
2023-07-07 23:11:11 +0900

    Update slack-notify v0.0.6

--- Formula/slack-notify.rb
+++ Formula/slack-notify.rb
@@ -1,13 +1,13 @@
 class SlackNotify < Formula
-  version '0.0.5'
+  version '0.0.6'
   homepage 'https://github.com/ebc-2in2crc/slack-notify'
   if OS.mac?
-    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.5/slack-notify_darwin_amd64.zip"
-    sha256 '0ebb878ca84269ea73609345457a8de74adf4ab1cb0f9c2df486d76a04bcf9e0'
+    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.6/slack-notify_darwin_amd64.zip"
+    sha256 '312b1b715cb505b14e6d79d033e7c663259dff30dd25085360c845597751569a'
   end
   if OS.linux?
-    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.5/slack-notify_linux_amd64.zip"
-    sha256 'a40b4a0c29d1f1baafd5a81bd0482e331b8afc4e8e235a4305b552610a045be3'
+    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.6/slack-notify_linux_amd64.zip"
+    sha256 'e9a81727724591e714d85d3d16049baa0bcb42140377f51c82582441c050c680'
   end
   head 'https://github.com/ebc-2in2crc/slack-notify.git'
 
