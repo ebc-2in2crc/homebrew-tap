68c607ce32ff552ec5a4ac31fecd07e75c15259e
ebc-2in2crc <y015h1n80@gmail.com>
2023-09-23 19:28:13 +0900

    Update slack-notify v0.0.9

--- Formula/slack-notify.rb
+++ Formula/slack-notify.rb
@@ -1,14 +1,14 @@
 class SlackNotify < Formula
   desc "Post Google Calendar events to Slack"
-  version '0.0.8'
+  version '0.0.9'
   homepage 'https://github.com/ebc-2in2crc/slack-notify'
   if OS.mac?
-    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.8/slack-notify_darwin_amd64.zip"
-    sha256 'c761b1d4d47cc9332a1cc44b4f81fee4cefcdfe887bf957ea80dca89d3f1dc16'
+    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.9/slack-notify_darwin_amd64.zip"
+    sha256 '86ecac374daecb3d61b8e6f491ad250dd8b04c90f6bfbdc8124b813e31f21dce'
   end
   if OS.linux?
-    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.8/slack-notify_linux_amd64.zip"
-    sha256 '1915ea3e95113c5d5828d4e3bf893946fa0426278aa9cdf87ce2f2c57d6679da'
+    url "https://github.com/ebc-2in2crc/slack-notify/releases/download/v0.0.9/slack-notify_linux_amd64.zip"
+    sha256 '9359780869445ff32253341c62e4504ada7fc30c4b6f56bf90e7b7522b7b8ae9'
   end
   head 'https://github.com/ebc-2in2crc/slack-notify.git'
 
