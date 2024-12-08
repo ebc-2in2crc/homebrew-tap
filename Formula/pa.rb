5a5e6639de75c0d5e215d22405fe8e99d7df95b6
ebc-2in2crc <y015h1n80@gmail.com>
2024-12-08 23:53:00 +0900

    Update pa v1.10.0

--- Formula/pa.rb
+++ Formula/pa.rb
@@ -1,9 +1,9 @@
 class Pa < Formula
   desc "Pixela (https://pixe.la/) CLI tools"
-  version '1.9.0'
+  version '1.10.0'
   homepage 'https://github.com/ebc-2in2crc/pa'
-  url "https://github.com/ebc-2in2crc/pa/releases/download/v1.9.0/pa_darwin_amd64.zip"
-  sha256 'e5de70185ebbcdf3895c5e6cddd6fe868880ec5698d17c9f79728be07bbe1ddf'
+  url "https://github.com/ebc-2in2crc/pa/releases/download/v1.10.0/pa_darwin_amd64.zip"
+  sha256 'cc5a20eb198bfed939b79aa6e233a0ea390e9ba6632a0f96583e7027f8b7adfa'
   head 'https://github.com/ebc-2in2crc/pa.git'
 
   head do
